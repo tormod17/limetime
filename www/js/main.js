a5649b01586fa637f4d35ac7035c7e3e09ae9d6b
tormod17 <tormodsmith@gmail.com>
2016-01-01 21:16:17 -0400

    adding lists to db

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,5 @@
 MemoryStore = [];
+var firebaseDb = new Firebase("https://limetime.firebaseio.com/");
 
 var app = {
 
@@ -15,7 +16,6 @@ var app = {
 
     getFirebase: function() {
 
-        var firebaseDb = new Firebase("https://limetime.firebaseio.com/");
         var MemoryStore = [];
 
         firebaseDb.on('value', function(snapshot) {
@@ -46,11 +46,10 @@ var app = {
 
             var html =
                 "<div class='header'><h1>Lime Time</h1>" +
-                "<div><button onclick='app.renderAddEvent()'>Add Event</button></div>" +
                 "<div>" +
                 "<ul class='menuButtons'><li><button onclick='app.thisWk()'>This Week</button></li>" +
                 "<li><button onclick='app.thisWknd()'>This Weekend</button></li>" +
-                "<li><button onclick='app.future()'>The future </a></li>" +
+                "<li><button onclick='app.renderAll()()'>All </a></li>" +
                 "</ul>";
             MemoryStore.forEach(function(event) {
                 html +=
@@ -59,76 +58,77 @@ var app = {
                     '<li>' + event.address + '</li>' +
                     '<li><a class="infoButton" onclick="app.renderEvent()">Info</a></li>' +
                     '</ul>';
+
             });
-            html += "</div>";
+            html += "</div><div><button onclick='app.renderAddEvent()'>Add Event</button></div>";
             $('body').html(html);
 
         });
     },
 
-    sortByTimeFunction : function(a,b){
-                    var dateA = new Date(a.dateTime).getTime();
-                    var dateB = new Date(b.dateTime).getTime();
-                    return dateA > dateB ? 1 : -1;
+    sortByTimeFunction: function(a, b) {
+        var dateA = new Date(a.dateTime).getTime();
+        var dateB = new Date(b.dateTime).getTime();
+        return dateA > dateB ? 1 : -1;
 
     },
 
     thisWk: function() {
-                MemoryStore.sort(this.sortByTimeFunction);
-                console.log('WK',MemoryStore);
+        MemoryStore.sort(this.sortByTimeFunction);
+        console.log('WK', MemoryStore);
 
-                var thisWk = MemoryStore.slice(0,7);
+        var thisWk = MemoryStore.slice(0, 7);
 
-                var html =
-                "<div class='header'><h1>Lime Time</h1>" +
-                "<div>"+
-                "<button onclick='app.renderHomeView()'>Back</button>" +
-                "<div>" +
-                "<ul class='menuButtons'><li><button onclick='app.thisWk()'>This Week</button></li>" +
-                "<li><button onclick='app.thisWknd()'>This Weekend</button></li>" +
-                "<li><button onclick='app.future()'>The future </a></li>" +
-                "</ul>";
+        var html =
+            "<div class='header'><h1>Lime Time</h1>" +
+            "<div>" +
+            "<button onclick='app.renderHomeView()'>Back</button>" +
+            "<div>" +
+            "<ul class='menuButtons'><li><button onclick='app.thisWk()'>This Week</button></li>" +
+            "<li><button onclick='app.thisWknd()'>This Weekend</button></li>" +
+            "<li><button onclick='app.renderAll()'>All </a></li>" +
+            "</ul>";
 
-                thisWk.forEach(function(event){
-                    html +=
-                    '<ul>' +
-                    '<li>' + event.eventName + '<li>' +
-                    '<li>' + event.address + '</li>' +
-                    '<li><a class="infoButton" onclick="app.renderEvent()">Info</a></li>' +
-                    '</ul>';
-                });
-               html += "</div>";
-               $('body').html(html);
+        thisWk.forEach(function(event) {
+            html +=
+                '<ul>' +
+                '<li>' + event.eventName + '<li>' +
+                '<li>' + event.address + '</li>' +
+                '<li><a class="infoButton" onclick="app.renderEvent()">Info</a></li>' +
+                '</ul>';
+        });
+        html += "</div>";
+        $('body').html(html);
     },
 
     thisWknd: function() {
-          MemoryStore.sort(this.sortByTimeFunction);
-          var thisWk = MemoryStore.slice(0,7);
-          var wknd= thisWk.filter(function(event){
-             // var day = new Date(event.dateTime).getDay();
-              return  event.dateTime.indexOf('FRI') > -1 || event.dateTime.indexOf('SAT') > -1 || event.dateTime.indexOf('SUN') > -1;
-          });
-          
-                var html =
-                "<div class='header'><h1>Lime Time</h1>" +
-                "<div>"+
-                "<button onclick='app.renderHomeView()'>Back</button>" +
-                "<div>" +
-                "<ul class='menuButtons'><li><button onclick='app.thisWk()'>This Week</button></li>" +
-                "<li><button onclick='app.thisWknd()'>This Weekend</button></li>" +
-                "<li><button onclick='app.future()'>The future </a></li>" +
-                "</ul>";
+        MemoryStore.sort(this.sortByTimeFunction);
+        var thisWk = MemoryStore.slice(0, 7);
+        var wknd = thisWk.filter(function(event) {
+            // var day = new Date(event.dateTime).getDay();
+            return event.dateTime.indexOf('FRI') > -1 || event.dateTime.indexOf('SAT') > -1 || event.dateTime.indexOf('SUN') > -1;
+        });
 
-                wknd.forEach(function(event){
-                    html +=
-                    '<ul>' +
-                    '<li>' + event.eventName + '<li>' +
-                    '<li>' + event.address + '</li>' +
-                    '<li><a class="infoButton" onclick="app.renderEvent()">Info</a></li>' +
-                    '</ul>';
-                });
-               html += "</div>";
-               $('body').html(html);
+        var html =
+            "<div class='header'><h1>Lime Time</h1>" +
+            "<div>" +
+            "<button onclick='app.renderHomeView()'>Back</button>" +
+            "<div>" +
+            "<ul class='menuButtons'><li><button onclick='app.thisWk()'>This Week</button></li>" +
+            "<li><button onclick='app.thisWknd()'>This Weekend</button></li>" +
+            "<li><button onclick='app.renderAll()()'>All </a></li>" +
+            "</ul>";
+
+        wknd.forEach(function(event) {
+            html +=
+                '<ul>' +
+                '<li>' + event.eventName + '<li>' +
+                '<li>' + event.address + '</li>' +
+                '<li><a class="infoButton" onclick="app.renderEvent()">Info</a></li>' +
+                '</ul>';
+        });
+        html += "</div>";
+        $('body').html(html);
 
 
 
@@ -136,30 +136,29 @@ var app = {
 
     },
 
-    future: function() {
-         MemoryStore.sort(this.sortByTimeFunction);
-         var future = MemoryStore.slice(7);
-         console.log('future',future);
-         var html =
-                "<div class='header'><h1>Lime Time</h1>" +
-                "<div>"+
-                "<button onclick='app.renderHomeView()'>Back</button>" +
-                "<div>" +
-                "<ul class='menuButtons'><li><button onclick='app.thisWk()'>This Week</button></li>" +
-                "<li><button onclick='app.thisWknd()'>This Weekend</button></li>" +
-                "<li><button onclick='app.future'>The future </a></li>" +
-                "</ul>";
+    renderAll: function() {
+        MemoryStore.sort(this.sortByTimeFunction);
 
-                future.forEach(function(event){
-                    html +=
-                    '<ul>' +
-                    '<li>' + event.eventName + '<li>' +
-                    '<li>' + event.address + '</li>' +
-                    '<li><a class="infoButton" onclick="app.renderEvent()">Info</a></li>' +
-                    '</ul>';
-                });
-               html += "</div>";
-               $('body').html(html);
+        var html =
+            "<div class='header'><h1>Lime Time</h1>" +
+            "<div>" +
+            "<button onclick='app.renderHomeView()'>Back</button>" +
+            "<div>" +
+            "<ul class='menuButtons'><li><button onclick='app.thisWk()'>This Week</button></li>" +
+            "<li><button onclick='app.thisWknd()'>This Weekend</button></li>" +
+            "<li><button onclick='app.renderAll()'>All </a></li>" +
+            "</ul>";
+
+        MemoryStore.forEach(function(event) {
+            html +=
+                '<ul>' +
+                '<li>' + event.eventName + '<li>' +
+                '<li>' + event.address + '</li>' +
+                '<li><a class="infoButton" onclick="app.renderEvent()">Info</a></li>' +
+                '</ul>';
+        });
+        html += "</div>";
+        $('body').html(html);
 
 
     },
@@ -172,7 +171,7 @@ var app = {
             "<div>" +
             "<ul class='menuButtons'><li><a>This Week</a></li>" +
             "<li><a>This Weekend</a></li>" +
-            "<li><a>The future </a></li>" +
+            "<li><a>All </a></li>" +
             "</ul>" +
             "<div class=' eventWrapper'>" +
             "<h3>" + MemoryStore[0].eventName + "</h3>" +
@@ -194,37 +193,70 @@ var app = {
 
             '<form role="form">' +
             '<label for="EventName">Event Name</label>' +
-            '<input type="text id="eventName" placeholder="Event Name">' +
+            '<input type="text" id="eventName" placeholder="Event Name" value="Pier one">' +
             '<br>' +
             '<label for="Address">Address</label>' +
-            '<input type="text id="address" placeholder="address">' +
+            '<input type="text" id="address" placeholder="address" value="chagaramus" >' +
             '<br>' +
             '<label for="Music">Music</label>' +
-            '<input type="text id="music" placeholder="music">' +
+            '<input type="text" id="music" placeholder="music" value="soca">' +
             '<br>' +
             '<label for="door policy">door policy</label>' +
-            '<input type="text id="doorPolicy" placeholder="door policy">' +
+            '<input type="text" id="doorPolicy" placeholder="door policy" value="cooler">' +
             '<br>' +
             '<label for="price">price</label>' +
-            '<input type="text id="price" placeholder="price">' +
+            '<input type="text" id="price" placeholder="price" value="300">' +
             '<br>' +
             '<label for="host">host</label>' +
-            '<input type="text id="host" placeholder="host">' +
+            '<input type="text" id="host" placeholder="host" value="Big Mumma">' +
             '<br>' +
             '<label for="cellPhone">cellPhone</label>' +
-            '<input type="text id="cellPhone" placeholder="cellPhone">' +
+            '<input type="text" id="cellPhone" placeholder="cellPhone" value="555-7777">' +
             '<br>' +
             '<label for="email">email</label>' +
-            '<input type="text id="email" placeholder="email">' +
+            '<input type="email" id="email" placeholder="email" value="lets@gmail.com">' +
             '<br>' +
-            '<input type="submit">' +
+            '<button type="button" onclick="app.sendFormData()">Submit</button>' +
             '</form>';
-
-
         html += "</div>";
         $('body').html(html);
 
     },
+
+    sendFormData: function() {
+        var eventName = document.getElementById('eventName').value;
+        var address = document.getElementById('address').value;
+        var music = document.getElementById('music').value;
+        var doorPolicy = document.getElementById('doorPolicy').value;
+        var price = document.getElementById('price').value;
+        var host = document.getElementById('host').value;
+        var cellPhone = document.getElementById('cellPhone').value;
+        var email = document.getElementById('email').value;
+
+
+       //var firebaseDb = new Firebase("https://limetime.firebaseio.com/");
+
+
+        firebaseDb.push({
+            'eventName': eventName,
+            'address': address,
+            'music': music,
+            'doorPolicy': doorPolicy,
+            'price': price,
+            'host': host,
+            'cellPhone': cellPhone,
+            'dateTime': new Date(),
+            'email': email
+
+        }, console.log('sending'));
+
+    },
+
+    sendToDB: function() {
+
+
+    },
+
     showAlert: function(message, title) {
         if (navigator.notification) {
             navigator.notification.alert(
